896db3347bbed8a8b27647e7e5d702d3e53f9c58
desync-o-tron <deltav@vt.edu>
2019-11-17 19:56:04 -0500

    adfasdf

--- install_buntu_pkgs.sh
+++ install_buntu_pkgs.sh
@@ -20,3 +20,6 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global user.name "desync-o-tron"
+git config --global user.name "deltav@vt.edu"
+
